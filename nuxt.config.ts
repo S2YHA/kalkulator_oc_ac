e5c596b99a2eb9d4d3168e9b826365862b9a7084
S2YHA <pawel.kaminski10@gmail.com>
2024-05-22 18:28:51 +0200

    add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  vite: {
+    server: {
+      fs: {
+        strict: false,
+      },
+    },
+  },
+});
